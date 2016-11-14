9f52236fcaf0be2257e41d2ddedbfbb033d971a5
Kushang Gajjar <g.kushang@gmail.com>
2016-11-14 14:08:37 -0500

    replicate cucumber featuer file for parallel run

---
+++ README.md
@@ -0,0 +1,2 @@
+## cucumber-replicate
+  Run Cucumber scenarios Parallel on Multiple browsers. Replicates the feature files based on the Browsers user wants to run
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var replicateTask = require('./lib/task');
+var replicate = require('./lib/replicate');
+
+module.exports = {
+  replicate: replicate,
+  replicateTask: replicateTask
+};
+
